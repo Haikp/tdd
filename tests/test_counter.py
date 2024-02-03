b491179282f6f0ed17c353ff7a35327d7a266507
Koi <kelvindo2002@gmail.com>
2024-02-02 22:03:22 -0800

    tdd done

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,56 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+class CounterTest(TestCase):
+    """Counter tests"""
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        # client = app.test_client()
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should update a counter"""
+        result = self.client.post('/counters/update')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        prevValue = result.json.get('update')
+        result = self.client.put('/counters/update')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.json.get('update'), prevValue + 1)
+
+    def test_false_counter_update(self):
+        """It should return an error for nonexistent counter updates"""
+        result = self.client.put('/counters/update')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_fetch_a_counter(self):
+        """It should fetch a counter"""
+        result = self.client.post('counters/fetch')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        fetchedCounter = self.client.get('counters/fetch')
+        self.assertEqual(fetchedCounter.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.json.get('fetch'), fetchedCounter.json.get('fetch'))
+
+    def test_false_fetch_counter(self):
+        """It should return an error for false fetches"""
+        fetchedCounter = self.client.get('counters/fetch')
+        self.assertEqual(fetchedCounter.status_code, status.HTTP_404_NOT_FOUND)
