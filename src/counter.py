360b3e9bb1b78563d4182dbab97b97cd4eda9e6a
Koi <kelvindo2002@gmail.com>
2024-02-07 23:17:29 -0800

    task 2 pushing to github

--- src/counter.py
+++ src/counter.py
@@ -9,27 +9,31 @@ COUNTERS = {}
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
+
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     app.logger.info(f"Request to update counter: {name}")
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     COUNTERS[name] = COUNTERS[name] + 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def fetch_counter(name):
     app.logger.info(f"Request to fetch counter: {name}")
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
